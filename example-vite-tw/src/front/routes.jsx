b34535cfa612c877b754f41e530373b43845e10c
relo-ga <relo.gallardo@gmail.com>
2025-09-18 18:20:14 -0500

    arreglando vista 6

--- example-vite-tw/src/front/routes.jsx
+++ example-vite-tw/src/front/routes.jsx
@@ -13,7 +13,7 @@ export const router = createBrowserRouter(
       // Root Route: All navigation will start from here.
         <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
             {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-            <Route path="/" element={<Home />} />
+            <Route index element={<Home />} />
             <Route path= "/productdetail/:id" element={<ProductDetail />} />
         </Route>
     )
